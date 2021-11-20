654ee72b032e28af529ec81546396d71ca0f8bd0
diarih <diarihza@gmail.com>
2021-11-20 12:30:58 +0700

    first done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,18 +1,51 @@
-import { AddTodo } from '../components/helper';
+import { AddTodo } from "../components/helper";
 
-test('Should add todo to the list', () => {
+test("Should add todo to the list", () => {
   // Arrange
+  const data = [
+    {
+      id: 1,
+      name: "Learn JSX",
+      isComplete: false,
+    },
+  ];
+
+  const insert = {
+    id: 1,
+    name: "Learn JSX",
+    isComplete: false,
+  };
+
+  const expected = [insert, ...data];
 
   // Act
+  const test = AddTodo(data, insert);
 
   // Assert
-})
+  expect(test).toStrictEqual(expected);
+});
 
-test('should not mutate the existing todo array', () => {
+test("should not mutate the existing todo array", () => {
   // Arrange
+  const data = [
+    {
+      id: 1,
+      name: "Learn JSX",
+      isComplete: false,
+    },
+  ];
+
+  const insert = {
+    id: 1,
+    name: "Learn JSX",
+    isComplete: false,
+  };
 
   // Act
+  const test = AddTodo(data, insert);
 
   // Assert
+  expect(test).not.toStrictEqual(data);
 
-})
+
+});
--- src/__tests__/Math.test.js
+++ src/__tests__/Math.test.js
@@ -2,32 +2,39 @@ import { Add, Substract, Multiplication } from '../Math'
 
 test('Return sum of two arguments', () => {
   // Arrange
-
+  const x = 3;
+  const y = 10;
+  const expected = 13;
   // Act
-
+  const test = Add(x,y)
 
   // Assert
+  expect(test).toBe(expected)
 
 })
 
 test('Return substraction of a - b', () => {
   // Arrange
-
+  const x = 5;
+  const y = 3;
+  const expected = 2;
 
   // Act
-
+  const test = Substract(x,y)
 
   // Assert
-
+  expect(test).toBe(expected)
 })
 
 test('Return multiplication of two arguments', () => {
   // Arrange
-
+  const x = 15;
+  const y = 5;
+  const expected = 75;
 
   // Act
-
+  const test = Multiplication(x,y)
 
   // Assert
-
+  expect(test).toBe(expected)
 })
